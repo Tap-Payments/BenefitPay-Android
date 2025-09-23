8f344adef097a7af6a74a06a7486903511bca795
AhlaamK-tap <57221514+AhlaamK-tap@users.noreply.github.com>
2025-09-23 07:07:16 +0300

    publish

--- benfitpay-sdk/src/main/java/company/tap/tapcardformkit/open/web_wrapper/BeneiftPayConfiguration.kt
+++ benfitpay-sdk/src/main/java/company/tap/tapcardformkit/open/web_wrapper/BeneiftPayConfiguration.kt
@@ -10,11 +10,11 @@ import company.tap.tapbenefitpay.open.AppLifecycleObserver
 import company.tap.tapbenefitpay.open.BenefitPayDataConfiguration
 import company.tap.tapbenefitpay.open.BenefitPayDataConfiguration.configurationsAsHashMap
 import company.tap.tapbenefitpay.open.TapBenefitPayStatusDelegate
-import company.tap.tapbenefitpay.open.web_wrapper.ApiServiceBenefit.BASE_URL
 import company.tap.tapnetworkkit.connection.NetworkApp
 import company.tap.tapnetworkkit.utils.CryptoUtil
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.launch
+import kotlin.coroutines.coroutineContext
 
 
 class BeneiftPayConfiguration {
@@ -26,9 +26,10 @@ class BeneiftPayConfiguration {
         private var testEncKey: String? = null
         private var prodEncKey: String? = null
         private var dynamicBaseUrlResponse: String? = null
+        private var context: Context? = null
        var configApiUrl : String = "https://mw-sdk.tap.company/v2/button/config"
         fun configureWithTapBenfitPayDictionaryConfiguration(
-            context: Context,
+            _context: Context,
             tapCardInputViewWeb: TapBenefitPay?,
             tapMapConfiguration: java.util.HashMap<String, Any>,
             tapBenefitPayStatusDelegate: TapBenefitPayStatusDelegate? = null
@@ -54,11 +55,12 @@ class BeneiftPayConfiguration {
 
             }*/
 
+            context = _context
             MainScope().launch {
                 getTapSDKConfigUrls(
                     tapMapConfiguration,
                     tapCardInputViewWeb,
-                    context,
+                    _context,
                     tapBenefitPayStatusDelegate
                 )
             }
@@ -94,8 +96,16 @@ class BeneiftPayConfiguration {
 
             } catch (e: Exception) {
              //   BASE_URL = urlWebStarter
-                testEncKey =  tapCardInputViewWeb?.context?.resources?.getString(R.string.enryptkeyTest)
-                prodEncKey = tapCardInputViewWeb?.context?.resources?.getString(R.string.enryptkeyProduction)
+                val resId = context.resources.getIdentifier(
+                    "enryptkeyTest", "string", context.packageName
+                )
+                val resIdprod = context.resources.getIdentifier(
+                    "enryptkeyProduction", "string", context.packageName
+                )
+                 testEncKey = context.getString(resId)
+                prodEncKey = context.getString(resIdprod)
+              //  testEncKey =  tapCardInputViewWeb?.context?.resources?.getString(R.string.enryptkeyTest)
+             //   prodEncKey = tapCardInputViewWeb?.context?.resources?.getString(R.string.enryptkeyProduction)
 
                 startSDKWithConfigs(
                     tapMapConfiguration,
@@ -154,25 +164,27 @@ class BeneiftPayConfiguration {
             publicKey: String?,
             tapCardInputViewWeb: TapBenefitPay?
         ): String? {
-            if (!testEncKey.isNullOrBlank() && !prodEncKey.isNullOrBlank()) {
-                return if (publicKey?.contains("test") == true) {
-                    // println("EncKey>>>>>" + testEncKey)
+            val ctx = tapCardInputViewWeb?.context ?: return null
+
+            return if (!testEncKey.isNullOrBlank() && !prodEncKey.isNullOrBlank()) {
+                if (publicKey?.contains("test", ignoreCase = true) == true) {
                     testEncKey
                 } else {
-                    //  println("EncKey<<<<<<" + prodEncKey)
                     prodEncKey
                 }
             } else {
-                //  println("EncKey<<<<<<>>>>>>>>>" + testEncKey)
-                return if (publicKey?.contains("test") == true) {
-                    tapCardInputViewWeb?.context?.resources?.getString(R.string.enryptkeyTest)
-                }else{
-                    tapCardInputViewWeb?.context?.resources?.getString(R.string.enryptkeyProduction)
+                if (publicKey?.contains("test", ignoreCase = true) == true) {
+                    val resId = ctx.resources.getIdentifier("enryptkeyTest", "string", ctx.packageName)
+                    testEncKey = if (resId != 0) ctx.getString(resId) else null
+                    testEncKey
+                } else {
+                    val resIdProd = ctx.resources.getIdentifier("enryptkeyProduction", "string", ctx.packageName)
+                    prodEncKey = if (resIdProd != 0) ctx.getString(resIdProd) else null
+                    prodEncKey
                 }
-
-
             }
         }
+
 
         private fun startSDKWithConfigs(
             tapMapConfiguration: java.util.HashMap<String, Any>,
