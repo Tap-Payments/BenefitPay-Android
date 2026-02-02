69aab3a643a21fb61396bac419b4622d57eb17fc
Ahlaam K <a.kazi@tap.company>
2026-02-02 16:29:16 +0300

    removed firebase crashlytics

--- app/src/main/java/com/example/benefitpay_android/MainActivity.kt
+++ app/src/main/java/com/example/benefitpay_android/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : AppCompatActivity() ,TapBenefitPayStatusDelegate{
     var currency:String = "BHD"
     var transactionReference:String = ""
     var postUrl:String = ""
-    var secretString = "sk_live_x28QGHEwiVet6yKq07zMOrjU"
+    var secretString = "XXXXXXXXXXXX"
    val number3digits:String = String.format("%.3f", amount)
    lateinit var dataTextView:TextView
 
